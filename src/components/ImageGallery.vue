f7457046593fa576afdc68443d0b41c74296d88a
henry25-ai <thongs2501@gmail.com>
2025-01-16 17:22:53 +0700

    UPDATE

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -3,6 +3,15 @@
         <div class="mb-4">
             <img :src="currentImage" alt="Gallery Image" class="w-[500px] h-[500px] object-cover border rounded-lg" />
         </div>
+        <!-- <input v-model="currentImage" type="text" placeholder="Enter image URL"
+            class="w-full px-4 py-2 border rounded-md" /> -->
+        <div class="mb-4 text-sm text-gray-600 flex items-center gap-2 w-full">
+            <input v-model="currentImage" type="text" placeholder="Enter image URL"
+                class="w-full px-4 py-2 border rounded-md" />
+            <button @click="copyToClipboard" class="px-2 py-1 text-white bg-gray-500 rounded hover:bg-gray-600">
+                Copy
+            </button>
+        </div>
         <div class="flex gap-4">
             <button @click="previousImage" class="cursor-pointer px-4 py-2 bg-blue-500 text-white rounded-md "
                 :disabled="currentIndex === 0" :class="{
@@ -31,7 +40,18 @@ import { useImageGallery } from "../composables/useImageGallery";
 const imageStore = useImageStore();
 const { images } = storeToRefs(imageStore);
 const { fetchImages } = imageStore;
+import { useToast } from "primevue/usetoast";
 
+const toast = useToast();
+
+const showNotification = () => {
+    toast.add({
+        severity: "success",
+        summary: "Success",
+        detail: "This is a success message.",
+        life: 3000,
+    });
+};
 // Sử dụng composable
 const {
     currentImage,
@@ -40,7 +60,12 @@ const {
     nextImage,
     initializeGallery,
 } = useImageGallery(fetchImages, images);
-
+const copyToClipboard = async () => {
+    try {
+        await navigator.clipboard.writeText(currentImage.value);
+    } catch (error) {
+    }
+};
 onMounted(() => {
     initializeGallery();
 });
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import "@/assets/main.css";
+import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
+
+// import "primevue/resources/themes/lara-light-indigo/theme.css";
+// import "primevue/resources/primevue.min.css";
+// import "primeicons/primeicons.css";
 const app = createApp(App);
+app.use(PrimeVue);
+app.use(ToastService);
 app.use(createPinia());
 app.mount("#app");
